b6f92ad5aafd0671f27341d47880f278b90e7cd4
csluo19550418 <30993375+luoluo19550418@users.noreply.github.com>
2020-07-01 18:13:19 +0800

    Create 找文件夹.py

---
+++ 程序模块&包/读写文件/找文件夹.py
@@ -0,0 +1,14 @@
+#filename: findfolder.py 遍历某路径下*02的文件夹
+# -*- coding: utf-8 -*- 
+
+import os 
+
+def returnfolder(rootdir):
+	list_dirs = os.walk(rootdir); folder=[]
+	for root,dirs,files in list_dirs:
+		for d in dirs:
+			if os.path.join(root,d)[-2:]=='02':  #os.path.join(root,d): rootdir路径下的文件
+				folder.append(os.path.join(root, d))
+	return folder
+
+folder=returnfolder('./')
