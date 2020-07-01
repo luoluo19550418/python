4626e7914be383342644d5cfdad88770d5f3c380
csluo19550418 <30993375+luoluo19550418@users.noreply.github.com>
2020-07-01 19:04:07 +0800

    Create pandas读文件.py

---
+++ 程序模块&包/读写文件/pandas读文件.py
@@ -0,0 +1,24 @@
+#读文件.py
+#pandas读文件，返回时间和数据
+
+import pandas as pd
+
+def returndata2(path, filename): #listdir is 所要读取的文件列表
+	dt,a20,a21=[],[],[]
+	dataset=pd.read_csv(path+filename,header=None,sep=' +',engine='python')
+	datetimes=pd.to_datetime(dataset.values[:,0]) + pd.to_timedelta(dataset.values[:,1],unit='h')  #date和time合成一列时间戳
+	dt.extend(datetimes); a20.extend(dataset.values[:,20]); a21.extend(dataset.values[:,21])
+	return dt,a20,a21
+
+dt,a20,a21=returndata2('./2020.02/', 'record20200211.txt')
+
+#读取多个文件（1个文件列表），追加到1个时间列表和1个数据列表
+def returndata(path, listdir): #listdir is 所要读取的文件列表
+	dt,a20,a21=[],[],[]
+	for filename in listdir:
+		dataset=pd.read_csv(path+filename,header=None,sep=' +',engine='python')
+		datetimes=pd.to_datetime(dataset.values[:,0]) + pd.to_timedelta(dataset.values[:,1],unit='h')  #date和time合成一列时间戳
+		dt.extend(datetimes); a20.extend(dataset.values[:,20]); a21.extend(dataset.values[:,21])
+	return dt,a20,a21
+
+dt,a20,a21=returndata('./2020.02/', listdir)
