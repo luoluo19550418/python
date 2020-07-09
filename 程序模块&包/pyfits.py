a433ad99fec64d824ad0d3a633dd887d0992d7d9
csluo19550418 <30993375+luoluo19550418@users.noreply.github.com>
2020-07-09 21:19:45 +0800

    Update pyfits.py

--- 程序模块&包/pyfits.py
+++ 程序模块&包/pyfits.py
@@ -7,7 +7,7 @@ from astropy.utils.data import download_file
 from astropy.io import fits                                      # We use fits to open the actual data file
 
 from astropy.utils import data
-#data.conf.remote_timeout = 60
+data.conf.remote_timeout = 60
 
 from spectral_cube import SpectralCube
 
@@ -18,9 +18,12 @@ from reproject import reproject_interp
 
 co_data = fits.open('1340+005U.fits')                            # Open the FITS file for reading
 cube = SpectralCube.read(co_data)                                # Initiate a SpectralCube
-hi_data.close()                                                  # Close the FITS file - we already read it in and don't need it anymore!
+co_data.close()                                                  # Close the FITS file - we already read it in and don't need it anymore!
 print(cube)                                                      # 查看cube头部信息
 cube[300, :, :].quicklook()                                      # Slice the cube along the spectral axis, and display a quick image
-cube[:, 75, 75].quicklook()                                      # Extract a single spectrum through the data cube
+cube[:, 46, 46].quicklook()                                      # Extract a single spectrum through the data cube
 
-sub_cube = cube.spectral(-300. *u.km / u.s, 300. *u.km / u.s)    
+lon_range = [133.75, 134.25] * u.deg                                   # Define desired latitude and longitude range
+lat_range = [0.25, 0.75] * u.deg
+sub_cube = cube.subcube(xlo=lon_range[0], xhi=lon_range[1], ylo=lat_range[0], yhi=lat_range[1])          # Create a sub_cube cut to these coordinates
+sub_cube_slab = sub_cube.spectral_slab(-100. *u.km / u.s, 100. *u.km / u.s)                              # Cut along the Spectral Axis
