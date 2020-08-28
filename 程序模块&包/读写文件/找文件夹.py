350c10c635c3ca293ae25d4e26715107399e1190
csluo19550418 <30993375+luoluo19550418@users.noreply.github.com>
2020-08-28 17:23:16 +0800

    Update 找文件夹.py

--- 程序模块&包/读写文件/找文件夹.py
+++ 程序模块&包/读写文件/找文件夹.py
@@ -1,14 +1,11 @@
 #filename: findfolder.py 遍历某路径下*02的文件夹
 # -*- coding: utf-8 -*- 
 
-import os 
-
-def returnfolder(rootdir): # rootdir is path（路径）
-	list_dirs = os.walk(rootdir); folder=[]
+def findfolders(path): # path 是路径
+	list_dirs = os.walk(path); folders=[]
 	for root,dirs,files in list_dirs:
 		for d in dirs:
-			if os.path.join(root,d)[-2:]=='02':  #os.path.join(root,d): rootdir路径下的文件
-				folder.append(os.path.join(root, d))
-	return folder
-
-folder=returnfolder('./')
+			if os.path.join(root,d)[-4:-2]=='20':  #os.path.join(root,d): path路径下的文件; #找20开头文件
+				folders.append(os.path.join(root, d))
+	return folders
+folders = findfolders('../rawdata/')
