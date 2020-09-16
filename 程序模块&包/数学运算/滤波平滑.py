85867bdb55dd7519f33936c212cc2027d4e6f235
csluo19550418 <30993375+luoluo19550418@users.noreply.github.com>
2020-09-17 01:31:45 +0800

    Update 滤波平滑.py

--- 程序模块&包/数学运算/滤波平滑.py
+++ 程序模块&包/数学运算/滤波平滑.py
@@ -6,3 +6,8 @@ delA1 = savgol_filter(delA1, 501, 1) #501是窗口长度，该值需为正奇整
 
 # 中值滤波是将每一点的值设置为该点某邻域窗口内的所有点的中值。这里的n就是邻域的大小
 delA1 = medfilt(delA1,501) # n=501
+
+
+# 参考：
+https://blog.csdn.net/weixin_42782150/article/details/107176500?utm_medium=distribute.pc_aggpage_search_result.none-task-blog-2~all~sobaiduend~default-2-107176500.nonecase&utm_term=python%E5%A6%82%E4%BD%95%E8%AE%A9%E6%9B%B2%E7%BA%BF%E5%B9%B3%E6%BB%91
+https://www.zhihu.com/question/24079334
